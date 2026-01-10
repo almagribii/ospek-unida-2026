fa3b16264bc8f052922a8e5cd52c38ebd2e85de6
mcqeems <mcqeemsofficial@gmail.com>
2026-01-10 21:13:15 +0700

    added config for biome

---
+++ src/app/home/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
+
+export default function Home() {
+	return <div className="h-[2000px] w-full">TEST</div>;
+}
