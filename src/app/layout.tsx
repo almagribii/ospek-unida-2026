2c1de1e846285544434a02de068535fadf0b95b5
mcqeems <mcqeemsofficial@gmail.com>
2026-01-11 00:18:01 +0700

    add logo in hero section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
 "use client";
 
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import gsap from "gsap";
 import { ReactLenis } from "lenis/react";
 import { useEffect, useRef } from "react";
-
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-});
 
 export default function RootLayout({
 	children,
@@ -37,11 +26,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<ReactLenis root />
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				{children}
-			</body>
+			<body>{children}</body>
 		</html>
 	);
 }
