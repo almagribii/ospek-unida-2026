721a5ff341d5b57acdf41fc69612f706270c6b57
mcqeems <mcqeemsofficial@gmail.com>
2026-01-13 00:48:43 +0700

    fix hydration error

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -2,10 +2,10 @@ import Navbar from "@/components/Navbar";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<html lang="en">
-			<Navbar/>
-			<body>{children}</body>
-		</html>
+		<>
+			<Navbar />
+			{children}
+		</>
 	);
 };
 
